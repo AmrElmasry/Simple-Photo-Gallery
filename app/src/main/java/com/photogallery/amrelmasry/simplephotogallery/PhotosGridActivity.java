c71d8ec095601132e14656335cae89e367ebff23
AmrElmasry <amrelmasry.me@gmail.com>
2016-09-07 11:06:50 +0200

    first commit

---
+++ app/src/main/java/com/photogallery/amrelmasry/simplephotogallery/PhotosGridActivity.java
@@ -0,0 +1,13 @@
+package com.photogallery.amrelmasry.simplephotogallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotosGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos_grid);
+    }
+}
